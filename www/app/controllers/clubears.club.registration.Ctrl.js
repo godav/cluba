6273872cb55b5a87f0df2ff2cf33ca4c0d448b1f
godav <godav@walla.com>
2018-05-07 06:42:28 -0700

    work on registration

--- www/app/controllers/clubears.club.registration.Ctrl.js
+++ www/app/controllers/clubears.club.registration.Ctrl.js
@@ -10,20 +10,27 @@ angular.module('app').controller('clubears.club.registration.Ctrl',
             $scope.friends = friends;
             $scope.currentPO = null;
 
+            var rtl_rx = /[\u0591-\u07FF]/;
+
+            $scope.checkLanguage = function (text) {
+                console.log("text", text);
+                console.log(rtl_rx.test(text) ? 'rtl' : 'ltr');
+                return rtl_rx.test(text) ? 'rtl' : 'ltr';
+            };
+
+
             console.log($scope.pos);
             $scope.signIn = function () {
                 currentEvent.$save().then(function () {
                     var groupId = currentEvent.groups;
                     console.log('groups', groupId);
                     EVENTS.addUserToEvent(currentEvent.$id, currentClub.$id, userObj, groupId);
-
                     $clubToast.show('הבקשה לרישום נשלחה', 'toaster-ancor', 'success');
                 });
             };
-
             $scope.cancel = function () {
                 $state.go('clubears.club.party');
             };
-
-        });
+        })
+        ;
   
